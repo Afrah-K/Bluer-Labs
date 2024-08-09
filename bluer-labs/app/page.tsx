f9312c550e9799405b5a734b05152ad9e9175f2d
Afrah <79169064+Afrah-K@users.noreply.github.com>
2024-08-09 08:46:55 +0000

    Created the mobile view by editing CardWithScroll

--- bluer-labs/app/page.tsx
+++ bluer-labs/app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 import Link from 'next/link';
 import EmailForm from '@/components/EmailForm';
-import { CardWithScroll } from "@/components/cardwithscroll";
+import { CardWithScroll } from "@/components/CardWithScroll";
 
 export default function Home() {
   return (
-    <div className="h-screen flex overflow-hidden">
-      <div className="flex-1 lg:pr-8 p-8 overflow-y-auto">
+    <div className="flex flex-col h-screen lg:flex-row lg:overflow-hidden">
+      <div className="flex-1 p-7 overflow-y-auto">
         <h1 className="text-left">
           <strong>I build AI software.</strong>
         </h1>
@@ -51,12 +51,18 @@ export default function Home() {
           <span>
             I&apos;ll reach out to you. Or book a call <Link href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ30LkwlTQDDIHy3dUn5d5hKdDGq4W8k90bH2fUtqfUlFabu8zTZKUud9doO5oXBRTvmGEyjoDr2?gv=true%27" className="font-bold underline">here</Link>.
           </span>
+          
+          {/* Mobile-only block */}
+          <div className="lg:hidden mt-4">
+            <h2 className="text-lg font-bold">Software I build</h2>
+            <CardWithScroll />
+          </div>
         </div>
       </div>
-      <div className="flex-1 lg:pl-8 flex flex-col justify-end overflow-y-auto">
-        <div className="p-2">
-          <CardWithScroll />
-        </div>
+      
+      {/* For PC View */}
+      <div className="hidden lg:flex lg:flex-1 p-2 lg:pl-8 overflow-y-auto">
+        <CardWithScroll />
       </div>
     </div>
   );
--- bluer-labs/components/CardWithScroll.tsx
+++ bluer-labs/components/CardWithScroll.tsx
@@ -9,37 +9,22 @@ const CardWithScroll: React.FC = () => {
     { title: 'Business Analytics', description: 'Transforms your sales data to identify trends and predict future performance empowering you to make smarter business decisions.', imageSrc: '/path/to/image5.jpg' },
     { title: 'Speech Recognition/Audio Processing', description: 'Perform tasks that transform text to speech, and speech to text.', imageSrc: '/path/to/image7.jpg' },
     { title: 'Computer Vision', description: 'See and recognize things better than humans, making your quality inspections faster and more accurate. Identify defects, monitor and analyze images better.', imageSrc: '/path/to/image8.jpg' },
-      { title: 'Custom AI Development', description: "Something new? There's nothing more I love than a good challenge", imageSrc: '/path/to/image1.jpg' },
-    ];
+    { title: 'Custom AI Development', description: "Something new? There's nothing more I love than a good challenge", imageSrc: '/path/to/image1.jpg' },
+  ];
 
   return (
-    <div className="h-screen flex flex-col">
-      <div className="h-full p-4">
-        <div className="flex flex-wrap space-y-2">
-          {[0, 2, 4, 6].map(index => (
-            <div key={index} className="w-full lg:w-full p-2 mt-4">
-              <div className="flex flex-row space-x-2">
-                <Card className="flex flex-row h-48 w-1/2">
-                  <div className="flex-2 p-4">
-                    <CardTitle>{cards[index].title}</CardTitle>
-                    <CardDescription>{cards[index].description}</CardDescription>
-                  </div>
-                  <div className="w-1/3 bg-cover bg-center" style={{ backgroundImage: `url(${cards[index].imageSrc})` }}></div>
-                </Card>
-                {index + 1 < cards.length && (
-                  <Card className="flex flex-row h-48 w-1/2">
-                    <div className="flex-2 p-4">
-                      <CardTitle>{cards[index + 1].title}</CardTitle>
-                      <CardDescription>{cards[index + 1].description}</CardDescription>
-                    </div>
-                    <div className="w-1/3 bg-cover bg-center" style={{ backgroundImage: `url(${cards[index + 1].imageSrc})` }}></div>
-                  </Card>
-                )}
-              </div>
+    <div className="flex flex-wrap">
+      {cards.map((card, index) => (
+        <div key={index} className="w-full lg:w-1/2 p-2">
+          <Card className="flex flex-col lg:flex-row h-48">
+            <div className="flex-2 p-4">
+              <CardTitle>{card.title}</CardTitle>
+              <CardDescription>{card.description}</CardDescription>
             </div>
-          ))}
+            <div className="w-full lg:w-1/3 bg-cover bg-center" style={{ backgroundImage: `url(${card.imageSrc})` }}></div>
+          </Card>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
